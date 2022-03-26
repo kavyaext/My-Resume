a0c9146316b742a672e4827822bf6659c0cdb2bb
kavyaext <56886850+kavyaext@users.noreply.github.com>
2022-03-26 20:45:10 +0530

    Initial Commit

---
+++ data/Kavya_Profile.js
@@ -0,0 +1,166 @@
+const profileData = {
+  title: 'Profile',
+  name: 'KAVYA P',
+  sub_title: 'SOFTWARE DEVELOPER',
+  about: 
+  {objective:'To secure a challenging position in a reputed organization where I can learn new skills, expand my knowledge and my learnings.To get an opportunity where I can make the best of my potential and contibute to the organizatios growth.',
+    contact: {
+      email: 'kavyakavya1091@gmail.com',
+      phone: '7092665572',
+      address: 'New York, USA'
+    }
+  },
+  links: [
+    { title: 'Github', src: 'https://github.com/kavyaext' },
+    { title: 'LinkedIn', src: 'https://www.linkedin.com/in/kavya-paramasivam-0340a7208/' }
+  ],
+  education: [
+    {
+      alma: 'St Josephs Institute Of Technology ,OMR Chennai',
+      duration: '2019 - 2023',
+      std: 'BTECH - IT',
+      score: '9.45 (Till 4th semester)'
+    },
+    {
+      alma: 'St Marys Matriculation Higher Secondary School',
+      duration: '2018 - 2019',
+      std: '12 th Grade',
+      score: '85%'
+    },
+    {
+      alma: 'St Marys Matriculation Higher Secondary School',
+      duration: '2016 - 2017',
+      std: '10 th Grade',
+      score: '95.4%'
+    }
+  ],
+  skills: [
+    {
+      //category: 'Lorem Ipsum',
+      topics: ['C Programming']
+    },
+    {
+      //category: 'Sed / Eiusmod',
+      topics: [
+        'Python (Basics)',
+       
+      ]
+    },
+    {
+      //category: 'Minim Veniam',
+      topics: ['HTML']
+    },
+
+    {
+      //category: 'Minim Veniam',
+      topics: ['CSS']
+    },
+    {
+      //category: 'Minim Veniam',
+      topics: ['Javascript (Basics)']
+    },
+    {
+      //category: 'Minim Veniam',
+      topics: ['Java']
+    }
+
+  ],
+  projects: [
+    {
+      title: 'RESUME MASTER',
+      duration: 'Apr 2022 - Mar 2022',
+      link: '- https://github.com/kavyaext/SMAIL-SOFTWARE.git',
+      desc: `- This is a website which is designed for creating resumes.`
+    },
+    {
+      title: 'PARKOSCOPE',
+      duration: 'Nov 2021 - Dec 2021',
+      link: '-https://github.com/kavyaext/Parkoscope',
+      desc: `-This website helps the user to find parking slot for the vehicle.`
+    },
+    {
+      title: 'SMAIL SOFTWARE',
+      duration: 'Mar 2021 - Nov 2021',
+      link: '- https://github.com/kavyaext/SMAIL-SOFTWARE.git',
+      desc: `- This is a mail software that is highly secured with End-to-End Encryption Algorithm. Hence called as SECURE
+      MAIL(SMAIL).`
+    },
+  ],
+  
+  internship: [
+    {
+      organization: 'TCR INNOVATION',
+      // title: `General`,
+      date: 'Oct 2021 - Dec 2021',
+      details: [
+        'Front-End developer.'
+      ]
+    },
+    {
+      organization: 'EXPOSYS DATA LAB',
+      //title: `Soldier`,
+      date: 'May 2021 - Jun 2021',
+      details: [
+        `Software development.`
+      ]
+    },
+    {
+    organization: 'NSIC (National Small Industries Corporation)',
+      //title: `Soldier`,
+      date: 'Nov 2020',
+      details: [
+        `App Development and IoT.`
+      ]
+    }
+
+  ],
+  certifications: [
+    {
+      title:'ShapeAI',
+      desc: `Python and Data Analytics Bootcamp.`,
+      date: 'Jun 2021'
+    },
+    {
+      title:'GUVI Geek Networks, IITM Research Park',
+      desc: `AI For India Guinness World Record.`,
+      date: 'Apr 2021'
+    },
+    {
+      title:'SOLOLEARN',
+      desc: `Java Programming`,
+      date: 'Feb 2021'
+    }
+  ],
+  
+  // events: [
+  //   {
+  //   title:'TCR INNOVATION',
+  //   body:[{
+  //         desc: `Frond-End developer.`,
+  //         date: 'Aug 1917'
+  //       }]
+  //   },
+  //   {
+  //     title:'EXPOSYS DATA LAB',
+  //     body:[{
+  //           desc:'SOFTWARE DEVELOPMENT.',
+  //           date: 'Jul 1945'
+  //         }]
+  //     },
+  //   {
+  //   title:'NSIC (National Small Industries Corporation)',
+  //   body:[{
+  //         desc:'App Development and IoT.',
+  //         date: 'Jul 1945'
+  //       }]
+  //   }
+  //   ]
+declaration:
+    {
+      desc:'I hereby declare that the above particulars of facts and information stated are true, correct and complete to the best of my belief and knowledge.',
+      place:'Place : Chennai'
+     
+  
+ }
+  
+}
